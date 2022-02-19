9bfb05900ab7b623f0510022524f897009227b9f
abdul <abdulazizpermana106@gmail.com>
2022-02-19 19:46:14 +0700

    add scroll

--- lib/page/signin_page.dart
+++ lib/page/signin_page.dart
@@ -20,95 +20,100 @@ class _SigninPageState extends State<SigninPage> {
           left: 24,
           right: 24,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Sign In",
-              style: tittleTextStyle,
-            ),
-            Text(
-              "Build Your Career",
-              style: subtittleTextStyle,
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Center(
-              child: Column(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Sign In",
+                style: tittleTextStyle,
+              ),
+              Text(
+                "Build Your Career",
+                style: subtittleTextStyle,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/images/image_page.png",
+                      width: 262,
+                      height: 240,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset(
-                    "assets/images/image_page.png",
-                    width: 262,
-                    height: 240,
+                  Text(
+                    "Email",
+                    style: tittleTextStyle,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  CustomTextForm(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Password",
+                    style: tittleTextStyle,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  CustomTextForm(),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    width: 400,
+                    height: 45,
+                    child: TextButton(
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => const SigninPage(),
+                        //   ),
+                        // );
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(66),
+                        ),
+                      ),
+                      child: Text(
+                        "Sign In",
+                        style: buttonTextStyle,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Center(
+                    child: Text(
+                      "Create New Account",
+                      style: tittleTextStyle,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
                   ),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Email",
-                  style: tittleTextStyle,
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                CustomTextForm(),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Password",
-                  style: tittleTextStyle,
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                CustomTextForm(),
-                const SizedBox(
-                  height: 40,
-                ),
-                Container(
-                  width: 400,
-                  height: 45,
-                  child: TextButton(
-                    onPressed: () {
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (context) => const SigninPage(),
-                      //   ),
-                      // );
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: primaryColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(66),
-                      ),
-                    ),
-                    child: Text(
-                      "Sign In",
-                      style: buttonTextStyle,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Center(
-                  child: Text(
-                    "Create New Account",
-                    style: tittleTextStyle,
-                  ),
-                )
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
